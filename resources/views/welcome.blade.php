f004cf50781a7d87e4c895d13af24ac50236b86d
raular250 <raular250@gmail.com>
2023-09-16 14:02:25 +0200

    Conexión a la BD realizada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,11 @@
         <h1>Page example</h1>
         <i class="bi bi-bell"></i>
         <p class="lead">Esta es una página de ejemplo</p>
+        @php
+            if(DB::connection()->getPdo()){
+                echo "conectado a la BD";
+            }
+        @endphp
     </div>
 </main>
 @endsection
