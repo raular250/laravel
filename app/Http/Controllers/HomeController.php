3b84faa7382527dbcc6775ffc7ba31356504a0b3
raular250 <raular250@gmail.com>
2023-09-16 15:02:42 +0200

    Añadido spatie permissions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,36 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $role = Role::create(['name' => 'writer']);
+        // $permission = Permission::create(['name' => 'edit articles']);
+
+        // $role->givePermissionTo($permission);
+
+        // $user=Auth::User();
+        // $roles = $user->getRoleNames(); // Returns a collection
+        // dump($roles);
+        // $permissionNames = $user->getPermissionNames(); // collection of name strings
+        // dump( $permissionNames);
+        // $permissions = $user->getPermissionsViaRoles();
+        // dump($permissions);
+
+        // $user->assignRole('writer');
+        // $roles = $user->getRoleNames(); // Returns a collection
+        // dump($roles);
+        // $permissionNames = $user->getPermissionNames(); // collection of name strings
+        // dump( $permissionNames);
+        // $permissions = $user->getPermissionsViaRoles();
+        // dump($permissions);
+
+        $user=Auth::User();
+        $user->removeRole('writer');
+        $role=Role::findByName('writer');
+        $role->revokePermissionTo('edit articles');
+        $permission=Permission::findByName('edit articles');
+        $permission->delete();
+        $role->delete();
+
+        dd('stop');
         return view('home');
     }
 }
